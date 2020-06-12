3ca10acf6db2a01ef30f05e4582558f3cfc6cfd4
Matt <matt@pop-os.localdomain>
2020-06-12 12:31:08 -0400

    Updates

---
+++ precache-manifest.ced633b756050e3a47499c97f0b970d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d922bdb2c0bd3d529c09a581611bd1e",
+    "url": "/githubpages/index.html"
+  },
+  {
+    "revision": "4f7453b982bd599364c6",
+    "url": "/githubpages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cc42dd01d2e54f1f69d4",
+    "url": "/githubpages/static/js/2.0b92a337.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/githubpages/static/js/2.0b92a337.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f7453b982bd599364c6",
+    "url": "/githubpages/static/js/main.28482d01.chunk.js"
+  },
+  {
+    "revision": "9099d9307efd0e14cfe9",
+    "url": "/githubpages/static/js/runtime-main.8d623f11.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/githubpages/static/media/logo.5d5d9eef.svg"
+  }
+]);
